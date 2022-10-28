8b3d16877fa193933b2e73f09ef2d5a898eb69e3
Darragh Walker <g00390383@atu.ie>
2022-10-28 13:49:19 +0200

    Setup Project class with all needed variables and getter and setter methods

--- src/main/java/ie/atu/project/Passenger.java
+++ src/main/java/ie/atu/project/Passenger.java
@@ -1,4 +1,61 @@
 package ie.atu.project;
 
 public class Passenger {
+    String title;
+    String name;
+    long id;
+    long phone;
+    int age;
+
+    public Passenger(String title, String name, long id, long phone, int age) {
+        setTitle(title);
+        setName(name);
+        setId(id);
+        setPhone(phone);
+        setAge(age);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getPhone() {
+        return phone;
+    }
+
+    public void setPhone(long phone) {
+        this.phone = phone;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
