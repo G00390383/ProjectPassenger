91af2fa1c0b22999f64e093041b7c226cdb9839a
Darragh Walker <g00390383@atu.ie>
2022-10-28 13:57:20 +0200

    created test of Name feature in test class and added Name feature to Passenger

--- src/test/java/ie/atu/project/PassengerTest.java
+++ src/test/java/ie/atu/project/PassengerTest.java
@@ -24,6 +24,18 @@ class PassengerTest {
         assertEquals("This is not a valid title", exMessage.getMessage());
     }
 
+    @Test
+    void testNamePass(){
+        Passenger myPassenger = new Passenger("Mr", "Darragh", 1234567891, 1233456789, 123);
+        assertEquals("Darragh", myPassenger.getName());
+    }
+
+    @Test
+    void testNameFail(){
+        Exception exMessage = assertThrows(IllegalArgumentException.class, () -> {new Passenger("Mr", "ls", 1235422173822L, 1222222223, 19);});
+        assertEquals("This is not a valid name", exMessage.getMessage());
+    }
+
     @AfterEach
     void tearDown() {
     }
